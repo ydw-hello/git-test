6e8511628dc4156515a7f1d40290f65cc8564b98
ydw <3285966843@qq.com>
2022-07-26 07:16:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author ydw
+ * @description
+ * @create 2022-07-26 7:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
