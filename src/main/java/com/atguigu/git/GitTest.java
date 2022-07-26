ae5df86976855127dedeec1bde2b244fb0c1979c
ydw <3285966843@qq.com>
2022-07-26 20:52:12 +0800

    master commit 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello master test");
         System.out.println("hello hotfix test");
+        System.out.println("push test");
     }
 }
